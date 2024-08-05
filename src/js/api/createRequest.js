0aa93e744ea5397b1642adabc50c1abc13017f24
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2024-08-05 14:34:23 +0300

    delete all comments

--- src/js/api/createRequest.js
+++ src/js/api/createRequest.js
@@ -1,24 +1,5 @@
 const localhost = 'https://ahj-diploma-backend-l54f.onrender.com';
 
-// const createRequest = async (data) => {
-//     const postOptions = {
-//         method: 'POST',
-//         body: JSON.stringify(data),
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//     }
-//     const response = await fetch('http://localhost:7070/?method=' + data.method, postOptions);
-
-//     if (response.ok) {
-//         const result = await response.json();
-//         return result;
-//       } else {
-//         console.error('Error:', response.status, response.statusText);
-//       }
-// };
-
-
 const createRequest = async (data) => {
     if (data.requestMethod === 'POST') {
         const postOptions = {
@@ -37,21 +18,6 @@ const createRequest = async (data) => {
             console.error('Error:', response.status, response.statusText);
         }
     }
-    // const postOptions = {
-    //     method: 'POST',
-    //     body: JSON.stringify(data),
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    // }
-    // const response = await fetch(localhost + '?method=' + data.method, postOptions);
-
-    // if (response.ok) {
-    //     const result = await response.json();
-    //     return result;
-    // } else {
-    //     console.error('Error:', response.status, response.statusText);
-    // }
 
     if (data.requestMethod === 'GET') {
 
